58deb374ac42413985af0c6bc21d71d51f545432
Akiranya <porridgen@gmail.com>
2023-02-19 14:47:11 +0800

    Remove unnecessary providers

--- towny-link/src/main/java/cc/mewcraft/townylink/TownyLink.java
+++ towny-link/src/main/java/cc/mewcraft/townylink/TownyLink.java
@@ -2,19 +2,13 @@ package cc.mewcraft.townylink;
 
 import cc.mewcraft.mewcore.message.Translations;
 import cc.mewcraft.townylink.config.LinkConfig;
-import cc.mewcraft.townylink.injector.ServerListenerProvider;
-import cc.mewcraft.townylink.injector.TownyListenerProvider;
-import cc.mewcraft.townylink.injector.TownyRepositoryProvider;
 import cc.mewcraft.townylink.listener.ServerListener;
 import cc.mewcraft.townylink.listener.TownyListener;
 import cc.mewcraft.townylink.messager.ConnectorMessenger;
 import cc.mewcraft.townylink.messager.DummyMessenger;
 import cc.mewcraft.townylink.messager.Messenger;
 import cc.mewcraft.townylink.object.TownyRepository;
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.inject.Scopes;
+import com.google.inject.*;
 import de.themoep.connectorplugin.bukkit.BukkitConnectorPlugin;
 import me.lucko.helper.plugin.ExtendedJavaPlugin;
 import org.bukkit.plugin.Plugin;
@@ -51,6 +45,10 @@ public class TownyLink extends ExtendedJavaPlugin {
         @Override protected void configure() {
             bind(TownyLink.class).toInstance(TownyLink.this);
 
+            bind(TownyListener.class).in(Scopes.SINGLETON);
+            bind(ServerListener.class).in(Scopes.SINGLETON);
+            bind(TownyRepository.class).in(Scopes.SINGLETON);
+
             Plugin connectorPlugin = getServer().getPluginManager().getPlugin("ConnectorPlugin");
             if (connectorPlugin == null) {
                 bind(Messenger.class).to(DummyMessenger.class).in(Scopes.SINGLETON);
@@ -58,10 +56,6 @@ public class TownyLink extends ExtendedJavaPlugin {
                 bind(Messenger.class).to(ConnectorMessenger.class).in(Scopes.SINGLETON);
                 bind(BukkitConnectorPlugin.class).toInstance((BukkitConnectorPlugin) connectorPlugin);
             }
-
-            bind(TownyRepository.class).toProvider(TownyRepositoryProvider.class).in(Scopes.SINGLETON);
-            bind(ServerListener.class).toProvider(ServerListenerProvider.class).in(Scopes.SINGLETON);
-            bind(TownyListener.class).toProvider(TownyListenerProvider.class).in(Scopes.SINGLETON);
         }
     }
 
